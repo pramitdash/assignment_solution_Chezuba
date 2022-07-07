44d6e04dbe7d934e75ef347308f432c0da82080f
Pramit Dash <80528499+pramitdash@users.noreply.github.com>
2022-07-07 20:12:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import json
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/1/queries/count/<DATE_PREFIX>", methods = ['GET'])
+def fetch_data(DATE_PREFIX):
+    prefix = DATE_PREFIX
+
+    #creating the dataframe from the .tsv file and add the columns name
+    df = pd.read_csv('hn_logs.tsv',sep = '\t', names=['date_time','url'])
+    
+    #converting the string date_time column to Date_time format by creating a new column
+    df['Date'] = pd.to_datetime(df['date_time'])
+
+    #setting the Date column as index of the dataframe
+    df.set_index('Date', inplace=True)
+      
+    #counting the numbers of record according to our given date_time range  
+    count = len(df[prefix].index)
+
+    #adding the count value to the json object 
+    result = {
+        "count" : count
+    }
+    #print(result)
+
+    return json.dumps(result)
+        
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
+    
+
+
+    
+
